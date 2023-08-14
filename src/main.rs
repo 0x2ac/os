835c7ad92dc897b6fb417d9032e662f71a9fe49a
Kartik Nair <kartikajitnair@gmail.com>
2023-08-14 23:10:21 +0400

    Exception handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
+#![feature(abi_x86_interrupt)]
 
+mod gdt;
+mod interrupts;
 mod vga_buffer;
 
 use core::panic::PanicInfo;
@@ -11,8 +14,14 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+fn init() {
+    interrupts::init_idt();
+    gdt::init();
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     println!("Hello world!");
     loop {}
 }
