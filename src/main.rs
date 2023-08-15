a8f5f3db7b95f4954c426bfdd43165f4149ab3ba
Kartik Nair <kartikajitnair@gmail.com>
2023-08-15 20:27:53 +0400

    Keyboard input

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,28 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
+
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    hlt_loop()
 }
 
 fn init() {
     interrupts::init_idt();
     gdt::init();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     init();
     println!("Hello world!");
-    loop {}
+    hlt_loop()
 }
