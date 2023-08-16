2d865483fce2b07154d66a73c7feb8511fefad9b
Kartik Nair <kartikajitnair@gmail.com>
2023-08-16 21:48:31 +0400

    Page mapping

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@
 
 mod gdt;
 mod interrupts;
+mod memory;
 mod vga_buffer;
 
 use core::panic::PanicInfo;
+
+use bootloader::{entry_point, BootInfo};
+use x86_64::{structures::paging::Translate, VirtAddr};
 
 pub fn hlt_loop() -> ! {
     loop {
@@ -27,9 +31,31 @@ fn init() {
     x86_64::instructions::interrupts::enable();
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     init();
     println!("Hello world!");
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
+
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
+    }
+
     hlt_loop()
 }
